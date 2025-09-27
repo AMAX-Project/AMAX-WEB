c0c3d0c4332a59f8683c1c8e7022d819993f53f3
AMAX-Project <amax@walldev.ru>
2025-09-27 17:31:37 +0600

    change config values

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Custom host
 export const server_uri = 'ws://127.0.0.1:1488/websocket';
 export const auth_title = 'MAX';
-export const auth_custom_text = 'Ответы на популярные вопросы';
-export const auth_custom_link = 'https://google.com';
+export const auth_custom_text = 'OnlinePhone Link';
+export const auth_custom_link = 'https://t.me/OnlinePhoneSIM_Bot';
 export const auth_bottom_text = 'Maked with ❤️ by WallD3v';
 export const auth_bottom_link = 'https://github.com/WallD3v/MaxWeb';
